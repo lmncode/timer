2c173975d384b4885b085d10dee8f9484ef2504c
Laman <lamanguliyeva@outlook.com>
2020-07-16 05:45:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {},
+  onTick() {},
+  onComplete() {},
+});
